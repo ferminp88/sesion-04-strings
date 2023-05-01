e9c8989f2b0025c7bd07f7fde9a5fa36adb6c3bf
Fermin <ferminp88@gmail.com>
2023-05-01 19:55:40 -0300

    first commit

---
+++ Ejercicio2.js
@@ -0,0 +1,11 @@
+let nombre ="fermin";
+let apellido="palacios";
+
+let estudiante= (nombre.concat(" ", apellido));
+let estudianteMayus= (nombre.toUpperCase);
+
+console.log(estudiante);
+console.log(estudianteMayus)
+
+
+
