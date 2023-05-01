67d1751b07fd71f83ec8967694a0e1286592b24e
Fermin <ferminp88@gmail.com>
2023-05-01 19:56:34 -0300

    ok

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -2,10 +2,28 @@ let nombre ="fermin";
 let apellido="palacios";
 
 let estudiante= (nombre.concat(" ", apellido));
-let estudianteMayus= (nombre.toUpperCase);
+let estudianteMayus=(estudiante.toUpperCase(estudiante));
+let estudianteMinus=(estudiante.toLowerCase(estudiante));
+let numletras= estudiante.length;
+let primeraletra= nombre.charAt(0);
+let ultimaletra= apellido.slice(-1);
+let sinespacios= estudiante.replace(/ /g,"");
+let esnombre =  estudiante.includes("fermin");
+
 
 console.log(estudiante);
-console.log(estudianteMayus)
+console.log(estudianteMayus);
+console.log(estudianteMinus);
+console.log(numletras);
+console.log(primeraletra);
+console.log(ultimaletra);
+console.log(sinespacios);
+console.log(esnombre);
+
+
+
+
+
 
 
 
